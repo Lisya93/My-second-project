818cb255f9b508ce57cfb6a715eda7bd826f99ad
Neto test <anna_laz.ru@mail.ru>
2025-01-21 21:23:59 +0300

    git remote add origin git@github.com:Lisya93/My-second-project.git

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 9000; // цена за билет
+        int bonus = 20; // бонусные рубли равные одной мили
+        int miles = ticket / bonus; // бонусные мили за билет
+
+        System.out.println(miles);
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+    }
+       }
